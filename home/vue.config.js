74c17ae6bb8e07fc9d79308720b4fa9b309bbf50
John-Luca <kutschera@newzcomp.com>
2023-02-19 23:56:34 +0000

    config changes that dont work anyways...

--- home/vue.config.js
+++ home/vue.config.js
@@ -19,6 +19,50 @@ module.exports = defineConfig({
     manifestOptions: {
       background_color: "#42b983",
       theme_color: "#42b983", // set the theme_color property
-    }
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: "#42b983", // set the appleMobileWebAppStatusBarStyle property
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png',
+    },
+    meta: [
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0',
+      },
+      {
+        name: 'description',
+        content: 'Simply create, share and edit goals, no registration required.',
+      },
+      {
+        property: 'og:title',
+        content: 'BY20XX',
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        name: 'twitter:title',
+        content: 'BY20XX',
+      },
+      {
+        name: 'twitter:description',
+        content: 'Simply create, share and edit goals, no registration required. Try it out, its free!',
+      },
+      {
+        name: 'twitter:image',
+        content: 'img/icons/by20xx.png',
+      },
+    ]
   },
 })
