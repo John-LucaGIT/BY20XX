ec94fbf5dacd503acee77a3c0583d7c6ba030baf
John-Luca <kutschera@newzcomp.com>
2023-02-19 13:23:02 +0000

    config updates

--- home/vue.config.js
+++ home/vue.config.js
@@ -6,6 +6,19 @@ module.exports = defineConfig({
       fallback: {
         crypto: false
       }
+    },
+  },
+  pwa: {
+    // appleMobileWebAppCapable: true,
+    name: 'BY20XX',
+    version: 1.0,
+    appleMobileWebAppCapable: 'yes',
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppStatusBarStyle: "#42b983", // set the appleMobileWebAppStatusBarStyle property
+    manifestOptions: {
+      background_color: "#42b983",
+      theme_color: "#42b983", // set the theme_color property
     }
-  }
+  },
 })
