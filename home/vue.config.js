19ead8b03aa9d64b82855099bac4375204e3fc63
John-Luca <kutschera@newzcomp.com>
2023-02-21 15:07:15 +0000

    update config

--- home/vue.config.js
+++ home/vue.config.js
@@ -12,60 +12,65 @@ module.exports = defineConfig({
     title: 'BY20XX',
     name: 'BY20XX',
     version: 1.0,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
     themeColor: "#42b983",
     msTileColor: "#42b983",
+
     manifestOptions: {
       background_color: "#42b983",
-      theme_color: "#42b983", // set the theme_color property
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black-translucent',
+      themeColor: "#42b983",
       icons: [
         {
-          src: './img/icons/android-chrome-192x192.png',
+          src: '../img/icons/android-chrome-192x192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: './img/icons/android-chrome-384x384.png',
+          src: '../img/icons/android-chrome-384x384.png',
           sizes: '384x384',
           type: 'image/png'
         },
         {
-          src: './img/icons/android-chrome-512x512.png',
+          src: '../img/icons/android-chrome-512x512.png',
           sizes: '512x512',
           type: 'image/png'
         },
         {
-          src: './img/icons/apple-touch-icon.png',
+          src: '../img/icons/apple-touch-icon.png',
           type: 'image/png'
         },
         {
           src: 'favicon.ico',
-          type: 'image/ico'
+          type: './image/ico'
         },
         {
-          src: './img/icons/favicon-16x16.png',
+          src: '../img/icons/favicon-16x16.png',
           type: 'image/png'
         },
         {
-          src: './img/icons/favicon-32x32.png',
+          src: '../img/icons/favicon-32x32.png',
           type: 'image/png'
         },
         {
-          src: './img/icons/safari-pinned-tab.svg',
+          src: '../img/icons/safari-pinned-tab.svg',
           type: 'image/svg'
         },
         {
-          src: './img/icons/mstile-150x150.png',
+          src: '../img/icons/mstile-150x150.png',
           type: 'image/png'
         }
       ],
     },
     iconPaths: {
       favicon: 'favicon.ico',
-      favicon32: './img/icons/favicon-32x32.png',
-      favicon16: './img/icons/favicon-16x16.png',
-      appleTouchIcon: './img/icons/apple-touch-icon.png',
-      maskIcon: './img/icons/safari-pinned-tab.svg',
-      msTileImage: './img/icons/mstile-150x150.png',
+      favicon32: '../img/icons/favicon-32x32.png',
+      favicon16: '../img/icons/favicon-16x16.png',
+      appleTouchIcon: '../img/icons/apple-touch-icon.png',
+      maskIcon: '../img/icons/safari-pinned-tab.svg',
+      msTileImage: '../img/icons/mstile-150x150.png',
     },
     meta: [
       {
@@ -94,7 +99,7 @@ module.exports = defineConfig({
       },
       {
         name: 'twitter:image',
-        content: './img/icons/by20xx.png',
+        content: '../img/icons/by20xx.png',
       },
     ]
   },
